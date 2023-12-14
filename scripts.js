11bd9fe1b1f588b0df3bd7a14eb7b3ebe81dbc69
hegeduscili <hegeduscecilia@gmail.com>
2023-12-14 18:06:01 +0100

    elsonek_leker_utana_kiirat

--- scripts.js
+++ scripts.js
@@ -2,8 +2,17 @@ const xhr = new XMLHttpRequest();
 
 xhr.onload = function(){
     const result = JSON.parse(xhr.response);
-    console.log(result);
+    console.log(result.value);
+
+
+        const buttonjoke = $('#tell_me');
+
+        buttonjoke.click(function(){
+        document.getElementById('joke_here').innerHTML = result.value;
+        });
 }
 
 xhr.open("GET",' https://api.chucknorris.io/jokes/random');
-xhr.send();
+xhr.send(); 
+
+
